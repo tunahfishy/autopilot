cc15449a5824ba627999e054b907e8f91f12e332
Tinah Hong <tinahh@mit.edu>
2023-11-18 23:56:25 -0800

    try visible elements

--- main.py
+++ main.py
@@ -3,37 +3,67 @@ import base64
 from playwright.sync_api import sync_playwright
 from dotenv import load_dotenv
 import os 
+from nav_tools import scroll_up, scroll_down, click_element, type, type_and_submit
 
 load_dotenv()
 
-def take_screenshot(url, save_path='screenshot.png'):
+def get_page_info(url, save_path='screenshot.png'):
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto(url)
 
+        # page.screenshot(path=save_path)
+        # elements = ['button', 'a', 'input', 'select', 'textarea', 'div[onclick]', 'span[onclick]']; 
+
+        x,y, distance = 300, 0, 200 #Example values
+        element_info = page.evaluate(f'''() => {{
+            const x = {x}, y = {y}, distance = {distance};
+            const elements = Array.from(document.querySelectorAll("a, button, input"));
+            return elements.filter(element => {{
+                const rect = element.getBoundingClientRect();
+
+                const inViewport = (
+                    rect.top >= 0 &&
+                    rect.left >= 0 &&
+                    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+                );
+
+                if (!inViewport) {{
+                    return false;
+                }}
+                const elementCenterX = rect.left + rect.width / 2;
+                const elementCenterY = rect.top + rect.height / 2;
+                const dx = elementCenterX - x;
+                const dy = elementCenterY - y;
+                if (Math.sqrt(dx * dx + dy * dy) <= distance) {{
+                    element.style.background = "blue";
+                    return true;
+                }}
+                return false;
+            }}).map(element => {{
+                let attributes = {{}};
+                for (let attr of element.attributes) {{
+                    attributes[attr.name] = attr.value;
+                }}
+                return {{tagName: element.tagName, attributes: attributes}};
+            }});
+        }}''')
         page.screenshot(path=save_path)
-        elements = page.query_selector_all("a, button, input")
-        element_info = [(element.get_attribute("tagName"), element.get_attribute("value") if element.get_attribute("tagName") and element.get_attribute("tagName").lower() == "input" else element.inner_text()) for element in elements]
-        
+
+        page.wait_for_timeout(2000)
+ 
+        # element_parents = [element.evaluate('node => node.parentElement.outerHTML') for element in elements]
         browser.close()
         return element_info
     
-def get_dom(url):
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto(url)
-        
-        body_html = page.evaluate('() => document.body.innerHTML')
-        browser.close()
-        return body_html
 
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
 
-def get_gpt_action(client, base64_image, prompt):
+def get_gpt_action(client, base64_image, prompt, elements):
     response = client.chat.completions.create(
     model="gpt-4-vision-preview",
     messages=[
@@ -46,12 +76,16 @@ def get_gpt_action(client, base64_image, prompt):
                 You are on the page shown in this image. You will be provided with a task that will require you to interact with the browser and navigate to different pages. 
                 For each step, you will think about which actions you should preform to complete the task. You have the following actions available to you:
 
-                1. Click on a link
-                2. Click on a button
-                3. Enter text into a text field
-                ... etc
+                1. CLICK_ATTRIBUTE_X - click on a link, button, or input with the attribute key "ATTRIBUTE" and the value "X"
+                2. TYPE_AND_SUBMIT - type text into an input and press enter
+                3. SCROLL_UP - 
+                4. SCROLL_DOWN
+                5. GO_BACK
+                6. END
 
-                You will reason about what actions to perform first, and only return the name of the first action that you want to do.
+                You are given the following elements on the page: {elements}
+                # You will reason about what actions to perform first, and only return the name of the first action that you want to do. 
+                Based on these, return the dictionary of the element that you want to click on first.
                 Here is your task: {prompt}
                 """
             },
@@ -74,10 +108,14 @@ def get_gpt_action(client, base64_image, prompt):
 
 if __name__ == "__main__":
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-    user_prompt = input("Please enter a prompt: ")
+    # user_prompt = input("Please enter a prompt: ")
 
+    elements = get_page_info("https://amazon.com/")
+    # print(len(elements))
 
-    take_screenshot("https://www.amazon.com/")
-    base64_image = encode_image("screenshot.png")
+    import json
+    print(len(elements), json.dumps(elements, indent=4))
+    # base64_image = encode_image("screenshot.png")
 
-    get_gpt_action(client, base64_image, user_prompt)
+    # get_gpt_action(client, base64_image, user_prompt, elements)
+
